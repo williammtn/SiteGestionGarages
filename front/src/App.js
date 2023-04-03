98989c0ddc11a252fadcd684b7034e81dde40d7e
unknown <aurelien_lariviere@ens.univ-artois.fr>
2023-04-03 12:54:00 +0200

    feature/nvabar (attente autentifcation avant modification)

--- front/src/App.js
+++ front/src/App.js
@@ -9,13 +9,12 @@ function Navigation() {
   return (
     <Navbar collapseOnSelect expand="lg" class="navbar" variant="dark">
   <Navbar.Brand ><Link to="/accueil"><img className="navbar-logo" src={process.env.PUBLIC_URL + '/logo_projet_react.png'} alt="Logo" /></Link></Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="navbar-composants">
-      <Nav.Link className="navbar-composant"><Link to="/accueil">accueil</Link></Nav.Link>
-      <Nav.Link className="navbar-composant"><Link to="/calendrier">calendrier</Link></Nav.Link>
-      <Nav.Link className="navbar-composant"><Link to="/listegarages">Liste des garages</Link></Nav.Link>
-      <Nav.Link className="navbar-composant"><Link to="/connexion">Connexion</Link></Nav.Link>
+      <Nav.Link className="navbar-composant" style={{ textDecoration: 'none' }}><Link to="/accueil">Accueil</Link></Nav.Link>
+      <Nav.Link className="navbar-composant" style={{ textDecoration: 'none' }}><Link to="/calendrier">Calendrier</Link></Nav.Link>
+      <Nav.Link className="navbar-composant" style={{ textDecoration: 'none' }}><Link to="/listegarages">Liste des garages</Link></Nav.Link>
+      <Nav.Link className="navbar-composant" style={{ textDecoration: 'none' }}><Link to="/connexion">Connexion</Link></Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>
@@ -29,8 +28,7 @@ function App() {
       <header className="App-header">
       <>
       <Navigation/>
-      <Routes>
-      </Routes>
+
     </>
       </header>
     </div>
