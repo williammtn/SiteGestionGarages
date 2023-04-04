57078f5fcfbb2968b10de08df05031ff0a3c19a0
William Mouton <william_mouton@ens.univ-artois.fr>
2023-04-04 14:20:16 +0200

    Formulaire d'enregistrement

--- back/router.js
+++ back/router.js
@@ -22,9 +22,9 @@ routes.get("/homeuh", (req, res) => {
 
 routes.post("/signup", (req, res) => {
   bcrypt.hash(req.body.password, saltRounds, (err, hash) => { 
-    db.get(" insert into users ( user_role, user_name, user_firstname , user_password, user_mail, user_tel )values($role,$name, $firstname,$password, $mail, $tel) returning user_id",
+    db.get(" insert into users (user_role, user_name, user_firstname , user_password, user_mail, user_tel )values($role,$name, $firstname,$password, $mail, $tel) returning user_id",
       {
-        $role: req.body.role,
+        $role : 0,
         $name: req.body.name,
         $firstname : req.body.firstname,
         $password: hash,
