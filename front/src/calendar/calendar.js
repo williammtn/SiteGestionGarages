e88c655da57606f54e245e358f6cb650daa7ceba
olivier.dufour62@yahoo.com <olivier_dufour@ens.univ-artois.fr>
2023-04-05 13:14:59 +0200

    feature/calendrier-form : Debut changement affichage

--- front/src/calendar/calendar.js
+++ front/src/calendar/calendar.js
@@ -5,10 +5,12 @@ import axios from "axios";
 import "../App.css";
 import { NamedTimeZoneImpl } from "@fullcalendar/core/internal";
 
-function MyCalendar() {
+
+function MyCalendar(props) {
   const [events, setEvents] = useState([]);
   const [garages, setGarages] = useState([]);
   const [benefits, setBenefits] = useState([]);
+  const [disponibilities, setDisponibilities] = useState([]);
   const [selectedGarageId, setSelectedGarageId] = useState("");
 
   useEffect(() => {
@@ -52,40 +54,67 @@ function MyCalendar() {
       .get(`http://localhost:8000/benefits/${selectedId}`)
       .then((response) => {
         setBenefits(response.data);
-        console.log(response.data)
+        
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    axios
+      .get(`http://localhost:8000/disponibilities/${selectedId}`)
+      .then((responsedispo) => {
+        setDisponibilities(responsedispo.data);
       })
       .catch((error) => {
         console.error(error);
       });
   };
+  const [benefit, setBenefit] = useState("");
+  const [selectedDisponibilityID, setSelectedDisponibilityID] = useState("");
 
-  const [garageId, setGarageId] = useState("");
-  const [benefitId, setBenefitId] = useState("");
-  const [date, setDate] = useState("");
 
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   const data = {
-  //     date: date,
-  //     name: titre,
-  //     duration: date
-  //     garageId: garageId,
-  //     benefitId: benefitId,
-  //     date: date,
-  //   };
-  //   axios.post("/appointment", data).then((response) => {
-  //     console.log(response);
-  //   });
-  // };
+
+
+const handleBenefitChange = (e) => {
+  const selectedBenefit = benefits[e.target.selectedIndex];
+  console.log(benefits[e.target.selectedIndex])
+  setBenefit(selectedBenefit.benefits_id);
+}
+
+
+
+  const handleOptionChange = (e) => {
+    setSelectedDisponibilityID(e.target.value);
+  };
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const data = {
+    userId: props.cookies.adf.id,
+    garageId: selectedGarageId,
+    benefits_id: benefit,
+    disponibility_id: selectedDisponibilityID
+  };
+
+console.log(data)
+  axios.post('/appointment', data)
+    .then((response) => {
+      alert('Appointment created successfully!');
+    })
+    .catch((error) => {
+      alert('Failed to create appointment.');
+    });
+};
 
   return (
     <div>
       <div className="form">
         <div>
-          <form>
+          <form className="col-8" method="post" onSubmit={handleSubmit}>
             <div>
               <select onChange={handleGarageChange}>
-              <option></option>
+                <option></option>
                 {garages.map((garage) => (
                   <option key={garage.garageId} value={garage.garageId}>
                     {garage.name}
@@ -93,17 +122,43 @@ function MyCalendar() {
                 ))}
               </select>
               <div>
-                <select>
+                <select onChange={handleBenefitChange}>
+                
                   {benefits.map((benefit) => (
-                    <option key={benefit.benefits_id} value={benefit.benetfits_id}>{benefit.benefits_name}</option>
+                <option
+                      key={benefit.benefits_id}
+                      value={benefit.benefits_id}
+                    >
+                      {benefit.benefits_name}
+                    </option>
                   ))}
                 </select>
               </div>
-              {/* <div>
-                <input type="hidden" name="titre" value="RDV de : "/>
-              </div> */}
+              <div className="row">
+                {disponibilities.map((disponibility) => (
+                  <>
+                    <div>
+                      <label key={disponibility.disponibility_id} for="dispo" className="mr-2">
+                        {disponibility.disponibility_date +
+                          " " +
+                          disponibility.start_hour +
+                          " - " +
+                          disponibility.end_hour}
+                      </label>
+                      <input onChange={handleOptionChange}
+                        key={disponibility.disponibility_id}
+                        type="radio"
+                        name="dispo"
+                        value={disponibility.disponibility_id}
+                      />
+                    </div>
+                  </>
+                ))}
+              </div>
               <div>
-              <button type="submit" class="btn btn-primary btn-block mb-4">Valider</button>
+                <button type="submit" class="btn btn-primary btn-block mb-4 col-3">
+                  Valider
+                </button>
               </div>
             </div>
           </form>
